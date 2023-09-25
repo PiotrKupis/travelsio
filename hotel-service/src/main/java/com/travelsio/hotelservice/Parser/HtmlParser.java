0250b7a5011468fc5686956b8633abd59eb24549
Piotr Kupis <piotr.kupis42@gmail.com>
2023-09-25 21:49:14 +0200

    Add parser for html pages

---
+++ hotel-service/src/main/java/com/travelsio/hotelservice/Parser/HtmlParser.java
@@ -0,0 +1,31 @@
+package com.travelsio.hotelservice.Parser;
+
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.ParseSettings;
+import org.jsoup.parser.Parser;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HtmlParser {
+
+    private final Parser parser;
+
+    public HtmlParser() {
+        ParseSettings parseSettings = new ParseSettings(true, true);
+        parser = Parser.htmlParser().settings(parseSettings);
+    }
+
+    public Document parse(String url) {
+        Document document = null;
+        try {
+            document = Jsoup.connect(url).parser(parser).get();
+        } catch (IOException e) {
+            // TODO add custom exception
+            e.printStackTrace();
+        }
+        return document;
+    }
+
+}
